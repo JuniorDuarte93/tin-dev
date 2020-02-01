1357f83026813a4c4a3d9231c10219afad2d9e58
JuniorDuarte93 <jr.duartee.93@gmail.com>
2020-02-01 19:51:00 -0300

    Web Finish

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -13,7 +13,7 @@ module.exports = {
             return res.status(400).json({error: 'Dev not exists'});
         }
 
-        if(targetDev.likes.includes(loggedDev._user)){
+        if(targetDev.likes.includes(loggedDev._id)){
             console.log('Deu Match')
         }
 
